f2523d0b33a84427c6e12c007a003c875b2176fb
Carlos Brighton <cbrghton@gmail.com>
2021-10-31 19:57:54 -0600

    :construction: :sparkles: Se agrega la estructura básica al programa

--- src/mx/ipn/Main.java
+++ src/mx/ipn/Main.java
@@ -1,8 +1,18 @@
 package mx.ipn;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String flag = "S";
+        Scanner scanner = new Scanner(System.in);
+
+        while(flag.equals("S") || flag.equals("s")) {
+            System.out.println("Bienvenido a la Actividad 13");
+
+            System.out.println("¿Desea repetir? S/N");
+            flag = scanner.nextLine();
+        }
     }
 }
